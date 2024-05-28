b914cdb63556e24759d98b3f6b5e5e95ceee098e
mohammodjuwelislam <mohammodjuwelislam@gmail.com>
2024-05-28 14:54:47 +0600

    slick slider

---
+++ js/slider.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+
+
+    // $('.single-item').slick({
+    //     // centerMode: true,
+    //     // centerPadding: '60px',
+    //     slidesToShow: 1,
+    //     arrows: true,
+    //     dots: true,
+    //     responsive: [
+    //       {
+    //         breakpoint: 768,
+    //         settings: {
+    //           dots: true,
+    //           centerMode: true,
+    //           centerPadding: '40px',
+    //           slidesToShow: 1
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           centerMode: true,
+    //           centerPadding: '40px',
+    //           slidesToShow: 1
+    //         }
+    //       }
+    //     ]
+    // });
+
+
+    // $('.single-item').slick({
+    //   dots: true,
+    //   slidesToShow: 1,
+    //   centerMode: true,
+    //   variableWidth: true
+    // });
+    $('.single-item').slick({
+      dots: true,
+    });
+});
+
